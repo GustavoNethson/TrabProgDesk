5a09dd992e6f023d7e28906a6d487dfcbb30c894
Gustavo Nethson <116599169+GustavoNethson@users.noreply.github.com>
2024-04-18 20:09:24 -0300

    Versão 1.0.1

---
+++ MecanicaMassaroMiamoto/src/main/java/com/mycompany/mecanicamassaromiamoto/MecanicaMassaroMiamoto.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.mecanicamassaromiamoto;
+
+/**
+ *
+ * @author  Gustavo Henrique da Silva Nethson
+ * @RA      173525-1
+ */
+public class MecanicaMassaroMiamoto {
+
+    public static void main(String[] args) {
+        new TelaInicial().setVisible(true);
+    }
+}
